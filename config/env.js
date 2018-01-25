cd62c27fd76106da7fd11af725b46e96121eb610
Jourdan Rodrigues <thiagojourdan@gmail.com>
2018-01-25 00:57:51 -0800

    :snowflake: Require setting an API_URL env var.

--- config/env.js
+++ config/env.js
@@ -8,14 +8,9 @@ const paths = require('./paths');
 delete require.cache[require.resolve('./paths')];
 
 const NODE_ENV = process.env.NODE_ENV;
-if (!NODE_ENV) {
-  throw new Error(
-    'The NODE_ENV environment variable is required but was not specified.'
-  );
-}
 
 // https://github.com/bkeepers/dotenv#what-other-env-files-can-i-use
-var dotenvFiles = [
+let dotEnvFiles = [
   `${paths.dotenv}.${NODE_ENV}.local`,
   `${paths.dotenv}.${NODE_ENV}`,
   // Don't include `.env.local` for `test` environment
@@ -30,13 +25,17 @@ var dotenvFiles = [
 // that have already been set.  Variable expansion is supported in .env files.
 // https://github.com/motdotla/dotenv
 // https://github.com/motdotla/dotenv-expand
-dotenvFiles.forEach(dotenvFile => {
-  if (fs.existsSync(dotenvFile)) {
+dotEnvFiles.forEach(dotEnvFile => {
+  if (fs.existsSync(dotEnvFile)) {
     require('dotenv-expand')(
-      require('dotenv').config({
-        path: dotenvFile,
-      })
+      require('dotenv').config({path: dotEnvFile})
     );
+  }
+});
+
+['NODE_ENV', 'API_URL'].forEach(key => {
+  if (!process.env[key]) {
+    throw new Error(`The ${key} environment variable is required but was not specified.`);
   }
 });
 
@@ -72,6 +71,7 @@ function getClientEnvironment(publicUrl) {
         // Useful for determining whether we’re running in production mode.
         // Most importantly, it switches React into the correct mode.
         NODE_ENV: process.env.NODE_ENV || 'development',
+        API_URL: process.env.API_URL,
         // Useful for resolving the correct path to static assets in `public`.
         // For example, <img src={process.env.PUBLIC_URL + '/img/logo.png'} />.
         // This should only be used as an escape hatch. Normally you would put
@@ -87,7 +87,7 @@ function getClientEnvironment(publicUrl) {
     }, {}),
   };
 
-  return { raw, stringified };
+  return {raw, stringified};
 }
 
 module.exports = getClientEnvironment;
